64675cf42a75ce8948cee67b24a7983f387acf55
Viktor Popp <viktorpopp.dev@gmail.com>
2026-03-04 20:33:50 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,190 @@
+use chrono::{DateTime, Local, TimeDelta};
+use cloudflare::{
+    endpoints::{
+        dns::dns::{
+            DnsContent, DnsRecord, ListDnsRecords, ListDnsRecordsOrder, ListDnsRecordsParams,
+            UpdateDnsRecord, UpdateDnsRecordParams,
+        },
+        zones::zone::{ListZones, ListZonesOrder, ListZonesParams, Zone},
+    },
+    framework::{
+        Environment, OrderDirection,
+        auth::Credentials,
+        client::{ClientConfig, blocking_api::HttpApiClient},
+    },
+};
+use croner::Cron;
+use std::{env, error::Error, net::Ipv4Addr, str::FromStr, thread::sleep, time::Duration};
+
+fn get_ip4() -> Ipv4Addr {
+    let ip4_raw = reqwest::blocking::get("https://checkip.amazonaws.com/")
+        .expect("failed to fetch IP address response")
+        .text()
+        .expect("failed to fetch IP address response");
+    ip4_raw
+        .as_str()
+        .trim()
+        .parse()
+        .expect("failed to pass IP address")
+}
+
+fn list_zones(api_client: &HttpApiClient) -> Vec<Zone> {
+    let endpoint = ListZones {
+        params: ListZonesParams {
+            name: None,
+            status: None,
+            page: None,
+            per_page: Some(50),
+            order: Some(ListZonesOrder::Name),
+            direction: Some(OrderDirection::Ascending),
+            search_match: None,
+        },
+    };
+
+    let response = api_client.request(&endpoint);
+
+    match response {
+        Ok(success) => success.result,
+        Err(error) => panic!("failed to list zones: {:#?}", error),
+    }
+}
+
+fn list_records(api_client: &HttpApiClient, id: &str) -> Vec<DnsRecord> {
+    let endpoint = ListDnsRecords {
+        zone_identifier: id,
+        params: ListDnsRecordsParams {
+            record_type: None,
+            name: None,
+            page: None,
+            per_page: Some(500), // I really hope people don't have more than that
+            order: Some(ListDnsRecordsOrder::Name),
+            direction: Some(OrderDirection::Ascending),
+            search_match: None,
+        },
+    };
+
+    let response = api_client.request(&endpoint);
+
+    match response {
+        Ok(success) => success.result,
+        Err(error) => panic!("failed to list DNS records from {}: {:#?}", id, error),
+    }
+}
+
+struct App {
+    ip4_domains: Vec<String>,
+    cache_expiration: Duration,
+    cached_last_time: DateTime<Local>,
+    cached_ip4: Ipv4Addr,
+    update_cron: Cron,
+    api_client: HttpApiClient,
+}
+
+impl App {
+    pub fn run(&mut self) -> Result<(), Box<dyn Error>> {
+        loop {
+            let now = Local::now();
+            let next = self.update_cron.find_next_occurrence(&now, false)?;
+
+            let ip4 = get_ip4();
+
+            if ip4 != self.cached_ip4
+                || now - self.cached_last_time > TimeDelta::from_std(self.cache_expiration)?
+            {
+                println!("updating records. IPv4: {}, time: {}", ip4, now);
+                self.cached_ip4 = ip4;
+                self.cached_last_time = now;
+                self.update_records()?;
+            }
+            sleep((next - now).to_std()?);
+        }
+    }
+
+    fn update_records(&mut self) -> Result<(), Box<dyn Error>> {
+        let zones = list_zones(&self.api_client);
+        for zone in zones {
+            let records = list_records(&self.api_client, &zone.id);
+            for record in records {
+                if self.ip4_domains.contains(&record.name) {
+                    self.update_ip4_record(&zone, &record);
+                }
+            }
+        }
+        Ok(())
+    }
+
+    fn update_ip4_record(&mut self, zone: &Zone, record: &DnsRecord) {
+        let endpoint = UpdateDnsRecord {
+            zone_identifier: &zone.id,
+            identifier: &record.id,
+            params: UpdateDnsRecordParams {
+                ttl: Some(record.ttl),
+                proxied: Some(record.proxied),
+                name: &record.name,
+                content: DnsContent::A {
+                    content: self.cached_ip4,
+                },
+            },
+        };
+
+        let response = self.api_client.request(&endpoint);
+
+        match response {
+            Ok(_) => {}
+            Err(_) => panic!("failed to update DNS record"),
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    dotenvy::dotenv()?;
+    let mut token = String::new();
+    let mut domains: Vec<String> = vec![];
+    let mut ip4_domains: Vec<String> = vec![];
+    let mut cache_expiration = Duration::from_mins(2);
+    let mut update_cron: Cron = Cron::from_str("* * * * *")?;
+
+    for (key, val) in env::vars() {
+        match key.as_str() {
+            "CLOUDFLARE_API_TOKEN" => token = val.clone(),
+            "DOMAINS" => {
+                domains = val.split(',').map(|s| s.trim().to_string()).collect();
+            }
+            "IP4_DOMAINS" => {
+                ip4_domains = val.split(',').map(|s| s.trim().to_string()).collect();
+            }
+            "CACHE_EXPIRATION" => {
+                cache_expiration = *val
+                    .parse::<humantime::Duration>()
+                    .expect("failed to parse CACHE_EXPIRATION")
+            }
+            "UPDATE_CRON" => update_cron = Cron::from_str(&key)?,
+            _ => {}
+        }
+    }
+
+    if token == String::new() {
+        panic!("could not find CLOUDFLARE_API_TOKEN variable in envirenment");
+    }
+
+    let credentials = Credentials::UserAuthToken {
+        token: token.clone(),
+    };
+
+    ip4_domains.extend(domains);
+
+    let mut app = App {
+        ip4_domains,
+        cache_expiration,
+        cached_last_time: DateTime::default(),
+        cached_ip4: Ipv4Addr::new(0, 0, 0, 0),
+        update_cron,
+        api_client: HttpApiClient::new(
+            credentials,
+            ClientConfig::default(),
+            Environment::Production,
+        )?,
+    };
+
+    app.run()
+}
